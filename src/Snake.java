8a11033ab81810bf446620fe957fe233f7bd74a1
Warmijak <Aguszka9*>
2021-03-21 21:18:06 +0100

    added run class, constructors in Snake

--- src/Snake.java
+++ src/Snake.java
@@ -1,7 +1,19 @@
+import java.util.Collections;
 import java.util.List;
 
 public class Snake {
     private Point head;
     private List<Point> body;
     private Direction direcion;
+
+    public Snake(Point head, List<Point> body, Direction direcion) {
+        this.head = head;
+        this.body = body;
+        this.direcion = direcion;
+    }
+
+    public Snake() {
+        this(new Point(0, 0), Collections.emptyList(), Direction.RIGHT);
+    }
+
 }
